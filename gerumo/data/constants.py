84355267fc1ab218c6488f587b4af6b20010ee3e
Sebastián Bórquez <sebastian.borquez@zippedi.com>
2021-11-24 00:41:14 -0300

    add Observations and Telescope structures

--- gerumo/data/constants.py
+++ gerumo/data/constants.py
@@ -4,32 +4,35 @@ Data and dataset constants
 """
 from astropy.units.cds import rad, eV
 import numpy as np
+from ..utils.structures import Telescope
 
 # Telescopes types
-TELESCOPES = ["LST_LSTCam", "MST_FlashCam", "SSTC"]
+TELESCOPES = {
+    "LST_LSTCam":  Telescope(None, None, None),
+    "MST_FlashCam": Telescope(None, None, None),
+    "SSTC": Telescope(None, None, None)
+}
 
 # Telescope array information
 HILLAS_PARAMETERS = [
     'hillas_intensity',
-	'hillas_x',
-	'hillas_y',
-	'hillas_r',
-	'hillas_phi',
-	'hillas_length',
-	'hillas_length_uncertainty',
-	'hillas_width',
-	'hillas_width_uncertainty',
-	'hillas_psi',
-	'hillas_skewness',
-	'hillas_kurtosis'
+    'hillas_x',
+    'hillas_y',
+    'hillas_r',
+    'hillas_phi',
+    'hillas_length',
+    'hillas_length_uncertainty',
+    'hillas_width',
+    'hillas_width_uncertainty',
+    'hillas_psi',
+    'hillas_skewness',
+    'hillas_kurtosis'
 ]
 TELESCOPE_FEATURES = [
     'pos_x',                # x array coordinate
     'pos_y',                # y array coordinate
     'pos_z',                # z array coordinate
 ] + HILLAS_PARAMETERS
-TELESCOPE_CAMERA   = {
-}
 
 # Regression Targets
 REGRESSION_TARGETS = ["alt", "az", "mc_energy", "log10_mc_energy"]
