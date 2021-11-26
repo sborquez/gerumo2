f6254a20554407341c6aab4b3faf84f1f5e8ca01
Sebastián Bórquez <sebastian.borquez@zippedi.com>
2021-11-26 01:03:37 -0300

    add output mappers

--- gerumo/config/defaults.py
+++ gerumo/config/defaults.py
@@ -12,17 +12,17 @@ _C.MODEL = CN()
 _C.MODEL.DEVICE = "cuda"
 _C.MODEL.TASK = Task.REGRESSION.name
 _C.MODEL.TELESCOPES = ["LST_LSTCam"]   # Telescope {type}_{camera_type}
-_C.MODEL.WEIGHTS = None             # Path  to a checkpoint file to be loaded to the model.
+_C.MODEL.WEIGHTS = None  # Path  to a checkpoint file to be loaded to the model
 # Models Loss
 _C.MODEL.LOSS = CN()
 _C.MODEL.LOSS.NAME = "MAE"
 _C.MODEL.LOSS.ARGS = []   # [Any, ...]
-_C.MODEL.LOSS.KWARGS = [] # [(str, Any), ...]
+_C.MODEL.LOSS.KWARGS = []  # [(str, Any), ...]
 # Model Architecture
 _C.MODEL.ARCHITECTURE = CN()
 _C.MODEL.ARCHITECTURE.NAME = "CNN"
 _C.MODEL.ARCHITECTURE.ARGS = []   # [Any, ...]
-_C.MODEL.ARCHITECTURE.KWARGS = [] # [(str, Any), ...]
+_C.MODEL.ARCHITECTURE.KWARGS = []  # [(str, Any), ...]
 # -----------------------------------------------------------------------------
 # Ensembler Stereo/Multi-Stereo
 # -----------------------------------------------------------------------------
@@ -30,7 +30,7 @@ _C.ENSEMBLER = CN()
 _C.ENSEMBLER.DEVICE = "cuda"
 _C.ENSEMBLER.NAME = "IntensityWeightedAverage"
 _C.ENSEMBLER.TELESCOPES = []    # Telescope {type}_{camera_type}
-_C.ENSEMBLER.ARCHITECTURES = [] # Checkpoint with architecture files 
+_C.ENSEMBLER.ARCHITECTURES = []  # Checkpoint with architecture files
 _C.ENSEMBLER.WEIGHTS = []       # Only weights checkpoint files
 # -----------------------------------------------------------------------------
 # Dataset
@@ -66,7 +66,8 @@ _C.GENERATOR.NAME = "MonoGenerator"
 _C.GENERATOR.NUM_WORKERS = 1
 # Shuffle Dataset
 _C.GENERATOR.ENABLE_SHUFFLE = True
-_C.GENERATOR.USE_STRICT_SHUFFLE = False # If shufle is enable, it keep same h5 file in a batch 
+# If shufle is enable, it keep same h5 file in a batch
+_C.GENERATOR.USE_STRICT_SHUFFLE = False
 # -----------------------------------------------------------------------------
 # Input
 # -----------------------------------------------------------------------------
@@ -97,9 +98,9 @@ _C.OUTPUT.REGRESSION.TARGETS_DOMAINS = [
     (1.15, 1.3),
     (-0.25, 0.25)
 ]
-# ---------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------
 # Solver for Mono training
-# ---------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------
 _C.SOLVER = CN()
 _C.SOLVER.BATCH_SIZE = 64
 _C.SOLVER.MAX_ITER = 200
@@ -107,21 +108,21 @@ _C.SOLVER.METHOD = "Adam"
 _C.SOLVER.BASE_LR = 0.001
 _C.SOLVER.NESTEROV = False
 _C.SOLVER.MOMENTUM = 0.9
-# ---------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------
 # Callbacks
-# ---------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------
 _C.CALLBACKS = CN()
 _C.CALLBACKS.EARLY_STOP_PATIENCE = 20
-_C.CALLBACKS.CHECKPOINTS_PERIOD =  10
-# ---------------------------------------------------------------------------- #
+_C.CALLBACKS.CHECKPOINTS_PERIOD = 10
+# ----------------------------------------------------------------------------
 # Monitor
-# ---------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------
 _C.MONITOR = CN()
 _C.MONITOR.SAVE_LOSS = True
-_C.MONITOR.VIS_PERIOD = 0 
-# ---------------------------------------------------------------------------- #
+_C.MONITOR.VIS_PERIOD = 0
+# ----------------------------------------------------------------------------
 # Misc options
-# ---------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------
 _C.EXPERIMENT_NAME = None
 # Directory where output files are written
 _C.OUTPUT_DIR = "./output"
