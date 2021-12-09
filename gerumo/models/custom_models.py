970191b94af6dad436f2a1391b64d4df72aa0d63
Sebastián Bórquez <sebastian.borquez@zippedi.com>
2021-12-09 01:46:25 -0300

    add cnn models (cls and reg)

--- gerumo/models/custom_models.py
+++ gerumo/models/custom_models.py
@@ -1,19 +1,100 @@
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
 from .base import BaseModel, MODEL_REGISTRY
+from .layers import HexConvLayer, ConvBlock
+from ..utils.structures import Task
 
 
 @MODEL_REGISTRY.register()
 class CNN(BaseModel):
 
     _KWARGS = [
-        'conv_kernel_sizes', 'compress_filters', 'compress_kernel_size',
-        'latent_variables', 'dense_layer_units'
+        'num_classes', 'num_targets',
+        'hexconv', 'conv_kernel_sizes', 'last_channels',
+        'dense_layer_units', 'kernel_regularizer_l2', 'activity_regularizer_l1'
     ]
 
     def architecture(self,
-                     conv_kernel_sizes=[5, 3, 3], compress_filters=256,
-                     compress_kernel_size=3, latent_variables=200,
-                     dense_layer_unitcs=[128, 128, 64]):
-        self.conv = conv_kernel_sizes
+                     num_classes=None, num_targets=None, hexconv=False,
+                     conv_kernel_sizes=[5, 3, 3], last_channels=512,
+                     dense_layer_units=[128, 128, 64],
+                     kernel_regularizer_l2=None, activity_regularizer_l1=None):
+        assert num_classes is not None or num_targets is not None
+        assert self._input_shape.has_image()
+        l2 = kernel_regularizer_l2
+        l1 = activity_regularizer_l1
+        # Inputs Components
+        self._input = []
+        self._input_img = layers.InputLayer(
+            name="images",
+            input_shape=self._input_shape.images_shape[1:])
+        self._input.append(self._input_img)
+        if self._input_shape.has_features():
+            self._input_features = layers.InputLayer(
+                name="features",
+                input_shape=self._input_shape.features_shape[1:])
+            self._input.append(self._input_features)
+        # Image Branch
+        if hexconv:
+            self._img_layer = HexConvLayer(filters=64, kernel_size=(3, 3))
+        else:
+            self._img_layer = layers.Conv2D(
+                filters=64, kernel_size=(3, 3), activation="relu",
+                kernel_initializer='he_uniform', padding='valid',
+                kernel_regularizer=regularizers.l1_l2(l1=l1, l2=l2),
+                bias_regularizer=regularizers.l2(l2),
+                activity_regularizer=regularizers.l1(l1))
+        self._img_dropout = layers.Dropout(0.25)
+        self._conv_blocks = [
+            ConvBlock(2**(7+i), k, l1=1e-5, l2=1e-4)
+            for i, k in enumerate(conv_kernel_sizes)
+        ]
+        self._compress_channels = layers.Conv2D(
+                filters=last_channels, kernel_size=1, activation="relu",
+                kernel_initializer='he_uniform', padding='valid',
+                kernel_regularizer=regularizers.l1_l2(l1=l1, l2=l2),
+                bias_regularizer=regularizers.l2(l2),
+                activity_regularizer=regularizers.l1(l1)
+        )
+        self._flatten = layers.Flatten()
+        # Concat Branch
+        if self._input_shape.has_features():
+            self._concat = layers.Concatenate()
+        # Logic Components
+        self._logic_blocks = [
+            (layers.Dense(units), layers.BatchNormalization(), layers.Activation('relu'))  # noqa
+            for units in dense_layer_units
+        ]
+        # Head Components
+        if self.task is Task.REGRESSION:
+            self.num_classes = None
+            self.num_targets = num_targets
+            self._head = layers.Dense(num_targets, activation="linear")
+        elif self.task is Task.CLASSIFICATION:
+            self.num_targets = None
+            self.num_classes = num_classes
+            self._head = layers.Dense(num_classes, activation="softmax")
+        else:
+            raise NotImplementedError
 
     def forward(self, X, training=False):
-        return super().forward(X, training=training)
+        if self._input_shape.has_features():
+            img_X, features_X = X
+        else:
+            img_X = X
+        front = self._input_img(img_X)
+        front = self._img_layer(front)
+        front = self._img_dropout(front, training=training)
+        for conv_block in self._conv_blocks:
+            front = conv_block(front, training=training)
+        front = self._compress_channels(front)
+        front = self._flatten(front)
+        if self._input_shape.has_features():
+            front2 = self._input_features(features_X)
+            front = self._concat([front, front2])
+        for logic_block in self._logic_blocks:
+            dense, bn, activation = logic_block
+            front = dense(front)
+            front = bn(front)
+            front = activation(front)
+        return self._head(front)
--- gerumo/models/layers.py
+++ gerumo/models/layers.py
@@ -5,9 +5,8 @@ Custom Layers
 Collection of models custom layers.
 """
 import tensorflow as tf
-from tensorflow.keras.layers import (
-    Layer, Conv2D, Lambda, Concatenate, MaxPooling2D, Reshape
-)
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
 
 
 def softmax(target, axis, name=None):
@@ -26,24 +25,25 @@ def softmax(target, axis, name=None):
     return softmax
 
 
-class HexConvLayer(Layer):
+class HexConvLayer(layers.Layer):
     def __init__(self, filters, kernel_size=(3, 3), name=None, reshape=None,
                  **kargs):
         super(HexConvLayer, self).__init__(name=name, **kargs)
         self.__name = name
         self.filters = filters
         self.kernel_size = kernel_size
-        self.conv2d = Conv2D(filters, kernel_size,
-                             kernel_initializer="he_uniform",
-                             padding="valid", activation="relu",
-                             strides=(2, 1))
-        self.left_split = Lambda(lambda x: x[:, 0, :-1, :, :])
-        self.right_split = Lambda(lambda x: x[:, 1, 1:, :, :])
-        self.concatenate = Concatenate(axis=2)
-        self.pooling = MaxPooling2D(pool_size=(2, 2))
+        self.conv2d = layers.Conv2D(
+            filters, kernel_size,
+            kernel_initializer="he_uniform",
+            padding="valid", activation="relu",
+            strides=(2, 1))
+        self.left_split = layers.Lambda(lambda x: x[:, 0, :-1, :, :])
+        self.right_split = layers.Lambda(lambda x: x[:, 1, 1:, :, :])
+        self.concatenate = layers.Concatenate(axis=2)
+        self.pooling = layers.MaxPooling2D(pool_size=(2, 2))
         self.__reshape = reshape
         if self.__reshape is not None:
-            self.reshape = Reshape(self.__reshape)
+            self.reshape = layers.Reshape(self.__reshape)
         # Infer shape in running time
         else:
             self.reshape = None
@@ -62,7 +62,7 @@ class HexConvLayer(Layer):
             self.__reshape = (
                 int(cshape[1])*2, int(cshape[2])//2, int(cshape[3])
                 )
-            self.reshape = Reshape(self.__reshape)
+            self.reshape = layers.Reshape(self.__reshape)
         front = self.reshape(front)
 
         return self.pooling(front)
@@ -74,5 +74,57 @@ class HexConvLayer(Layer):
             "kernel_size": self.kernel_size,
             "name": self.__name,
             "reshape": self.__reshape
+        })
+        return config
+
+
+class ConvBlock(layers.Layer):
+    def __init__(self, filters, kernel_size, l1=1e-5, l2=1e-4, **kwargs):
+        super(ConvBlock, self).__init__(**kwargs)
+        # Config
+        self._kernel_size = kernel_size
+        self._filters = filters
+        self._l1 = l1
+        self._l2 = l2
+        # Layers
+        self._conv1 = layers.Conv2D(
+            filters=filters, kernel_size=kernel_size, padding='same',
+            kernel_initializer="he_uniform",
+            kernel_regularizer=regularizers.l1_l2(l1=l1, l2=l2),
+            bias_regularizer=regularizers.l2(l2),
+            activity_regularizer=regularizers.l1(l1)
+        )
+        self._bn1 = layers.BatchNormalization()
+        self._activation1 = layers.Activation('relu')
+        self._drop1 = layers.Dropout(0.25)
+        self._conv2 = layers.Conv2D(
+            filters=filters, kernel_size=kernel_size, padding='same',
+            kernel_initializer="he_uniform",
+            kernel_regularizer=regularizers.l1_l2(l1=l1, l2=l2),
+            bias_regularizer=regularizers.l2(l2),
+            activity_regularizer=regularizers.l1(l1)
+        )
+        self._bn2 = layers.BatchNormalization()
+        self._activation2 = layers.Activation('relu')
+        self._maxpool = layers.MaxPool2D()
+
+    def call(self, inputs, training=False):
+        x = self._conv1(inputs)
+        x = self._bn1(x)
+        x = self._activation1(x)
+        x = self._drop1(x, training=training)
+        x = self._conv2(x)
+        x = self._bn2(x)
+        x = self._activation2(x)
+        x = self._maxpool(x)
+        return x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'kernel_size': self._kernel_size,
+            'filters': self._filters,
+            'l1': self._l1,
+            'l2': self._l2
         })
         return config
